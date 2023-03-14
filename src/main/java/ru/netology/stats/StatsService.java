37569471b87d56faed6ce07c3c54b1a36ff09704
Alexander Krasilnikov <i@krasilnikov-1.ru>
2023-03-14 21:58:27 +0300

    New commit

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,2 +1,70 @@
-package ru.netology.stats;public class StatsService {
+package ru.netology.stats;
+
+public class StatsService {
+    public long sumAllSales(long[] sales) {
+        long sumSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sumSales = sumSales + sales[i];
+
+        }
+        return sumSales;
+    }
+
+    public long averageSales(long[] sales) {
+        long sumSales = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sumSales = sumSales + sales[i];
+
+        }
+        return sumSales / 12;
+    }
+
+    public int maxSaleMonth(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSaleMonth(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int belowAverageSales(long[]sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            if (sale < averageSales(sales)) {
+                sum++;
+            }
+        }
+        return sum;
+
+    }
+    public int aboveAverageSales(long[]sales) {
+        int sum = 0;
+        for (long sale : sales) {
+            if (sale > averageSales(sales)) {
+                sum++;
+            }
+        }
+        return sum;
+
+
+
+
+}
+
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,2 +1,78 @@
-package ru.netology.stats;public class StatsServiceTest {
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void sumAllSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sumAllSales(sales);
+        long expected = 180;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void averageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.averageSales(sales);
+        long expected = 15;
+
+        Assertions.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void maxSaleMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxSaleMonth(sales);
+        long expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSaleMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minSaleMonth(sales);
+        long expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void belowAverageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.belowAverageSales(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void aboveAverageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.aboveAverageSales(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
